5752768155bd6c1f37976022c76279e43f2d6e74
programing360 <fhlimon360@gmail.com>
2025-01-14 23:36:04 +0600

    added rectangle javascript section

---
+++ images/js/rectangle.js
@@ -0,0 +1,19 @@
+function rectangleTextValue(){
+    const rectangleWidth = document.getElementById('rectangle-width')
+    const width = rectangleWidth.value;
+    const rectangleText = parseFloat(width)
+    console.log('paisi', rectangleText);
+    // rectangle length
+    const rectangleLength = document.getElementById('rectangle-length')
+    const length = rectangleLength.value;
+    const rectangleTextValue = parseFloat(length)
+    console.log(rectangleTextValue);
+    // display rectangle area
+
+    const areaValue = rectangleText * rectangleTextValue;
+    // console.log(areaValue)
+    
+    const rectangleSpan = document.getElementById('rectangle-span')
+    rectangleSpan.innerText = areaValue
+    
+}
